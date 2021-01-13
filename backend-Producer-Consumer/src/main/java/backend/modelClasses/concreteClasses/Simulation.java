ff2515161ad4f56c28275e155aaf29c8aa2395cb
AbdelrahmanElmeniawy <52868473+AbdelrahmanElmeniawy@users.noreply.github.com>
2021-01-13 09:28:56 +0200

    test

--- backend-Producer-Consumer/src/main/java/backend/modelClasses/concreteClasses/Simulation.java
+++ backend-Producer-Consumer/src/main/java/backend/modelClasses/concreteClasses/Simulation.java
@@ -1,9 +1,23 @@
 package backend.modelClasses.concreteClasses;
 
+import javax.crypto.Mac;
+
 public class Simulation {
 
-    public void startSimulation() {
+    public void startSimulation(int numberOfProducts) {
         Diagram diagram = Diagram.getInstance();
+        SystemBuilder systemBuilder = new SystemBuilder();
+        systemBuilder.systemBuild(numberOfProducts);
+        /*
+         * int i = 0; for (Queue q : diagram.getQueues()) { System.out.print("Queue " +
+         * i + ": "); for (Machine m : q.getMachinesList()) { int j = 0; for (Machine
+         * target : diagram.getMachines()) { if (m.getID() == target.getID()) {
+         * System.out.print(j + " "); break; } j++; } System.out.println(); } i++; } i =
+         * 0; for (Machine m : diagram.getMachines()) { System.out.print("Machine " + i
+         * + ": "); Queue q = m.getOutputQue(); int j = 0; for (Queue target :
+         * diagram.getQueues()) { if (q.getID() == target.getID()) { System.out.print(j
+         * + " "); break; } j++; } System.out.println(); i++; }
+         */
         SnapShot snapShot = new SnapShot();
         snapShot.save(diagram);
         Thread threads = new Thread(new ProductGenerator());
