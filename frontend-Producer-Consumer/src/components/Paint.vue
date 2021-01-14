fd13c0633f6a3ef9b707008c7773774eee03c684
mina-henen <mina-henen@users.noreply.github.com>
2021-01-14 17:36:21 +0200

    Update Paint.vue

--- frontend-Producer-Consumer/src/components/Paint.vue
+++ frontend-Producer-Consumer/src/components/Paint.vue
@@ -291,14 +291,16 @@ export default {
             });
             this.updateDiagram();
         },
-        async updateDiagram() {                
-                setTimeout(async()=>{ console.log("Test Update")
+        async updateDiagram() {
+            for (let index = 0; index < 4; index++) {
+                console.log("Test Update")
                 var response = await axios.get("http://localhost:8095/get/updates/");
                 this.diagram = (response.data);
                 console.log(this.diagram);
                 this.clear();
                 this.operation=null;
-                this.drawBoard(); }, 1000);
+                this.drawBoard();
+            }
             
         },
         /************************************************************************ OLD CODE *********************************************************************/
